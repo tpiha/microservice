7498d0e913d92d013e91cf6499e28a6e06f61cf5
tpiha <piha.tihomir@gmail.com>
2021-02-22 04:34:21 +0100

    removed extra code

--- microservice_test.go
+++ microservice_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"strings"
@@ -74,21 +73,6 @@ func checkHealthCount() bool {
 
 func TestMicroservice(t *testing.T) {
 	client = resty.New()
-
-	client.
-		// Set retry count to non zero to enable retries
-		SetRetryCount(3).
-		// You can override initial retry wait time.
-		// Default is 100 milliseconds.
-		SetRetryWaitTime(5 * time.Second).
-		// MaxWaitTime can be overridden as well.
-		// Default is 2 seconds.
-		SetRetryMaxWaitTime(20 * time.Second).
-		// SetRetryAfter sets callback to calculate wait time between retries.
-		// Default (nil) implies exponential backoff with jitter
-		SetRetryAfter(func(client *resty.Client, resp *resty.Response) (time.Duration, error) {
-			return 0, errors.New("quota exceeded")
-		})
 
 	if checkHealth() {
 		fmt.Printf("Testing microservice...\n")
