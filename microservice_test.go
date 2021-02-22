a7084bc8fcbcb7e8e9fa9694e6f2397ae1642d07
tpiha <piha.tihomir@gmail.com>
2021-02-22 23:54:37 +0100

    update

--- microservice_test.go
+++ microservice_test.go
@@ -60,7 +60,7 @@ func checkHealthCount() bool {
 
 func TestMicroservice(t *testing.T) {
 	os.Remove("microservice.db")
-	cmd := exec.Command("microservice")
+	cmd := exec.Command("./microservice")
 	go cmd.CombinedOutput()
 
 	time.Sleep(time.Second)
